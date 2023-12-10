640271d3dfd9c4a21288de0732a8abaef7c83b90
Deep Duggal <7544238+deepduggal@users.noreply.github.com>
2023-12-10 17:01:52 -0500

    Create graphs-for-testing.js

---
+++ graphs/testing/graphs-for-testing.js
@@ -0,0 +1,15 @@
+/**
+ * Make test data
+ */
+
+const testGraphs = {
+  weighted: null,
+  unweighted: null,
+  directed: null,
+  undirected: null,
+  // trees
+  tree: null,
+  bfs: null
+};
+
+export testGraphs;
