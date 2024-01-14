85e95b07efc900e96add3c9c55412e948c3c2aaa
Deep Duggal <7544238+deepduggal@users.noreply.github.com>
2024-01-14 15:04:11 -0500

    started impl for StaticArray

--- src/arrays/StaticArray.ts
+++ src/arrays/StaticArray.ts
@@ -1,4 +1,5 @@
-interface Array<T> {
+interface IArray<T> {
+  length: number;
   /**
    * Access an element at a given index.
    */
@@ -13,6 +14,57 @@ interface Array<T> {
   pop(): T;
 }
 
-interface StaticArray<T> extends Array<T> {
+interface StaticArray<T> extends IArray<T> {
 
+}
+
+/**
+ * A fixed-length array, that doesn't auto resize, whose values are the same type.
+ */
+class StaticArray<T> implements StaticArray<T> {
+  // type ArrayIndex = number;
+  // type ArrayValue = T | undefined;
+  private data: Record<string, T | undefined>;
+  public length: number;
+
+  constructor(size: number) {
+    this.data = {};
+    Object.seal(this.data);
+    this.length = size;
+
+    // Allocate pointers/memory
+    for (let i = 0; i < this.length; i++) {
+      this.data[i.toString()] = undefined;
+    }
+  }
+
+  get(index: number) {
+    if (this.has(index)) {
+      const indexStr = index.toString();
+      return this.data[indexStr];
+    }
+    return undefined;
+  }
+
+  set(index: number, value: T | undefined) {
+    if (this.has(index)) {
+      const indexStr = index.toString();
+      this.data[indexStr] = value;
+    } else {
+      throw RangeError('Index doesn\'t exist');
+    }
+  }
+
+  has(index: number) {
+    if (this.data[index.toString()]) {
+      return true;
+    }
+    return false;
+  }
+  // push() {
+
+  // }
+  // pop() {
+
+  // }
 }
